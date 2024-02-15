bb5d4052d6b183b46fd00476969766edded299f1
KagurazakaMiyabi (神楽坂雅詩) <miyabi@uuu.moe>
2024-02-15 20:45:23 +0800

    创建测试配置1

--- test/data/test-project2/index.ts
+++ test/data/test-project2/index.ts
@@ -1,5 +1,7 @@
 const ps: HTMLCollectionOf<HTMLParagraphElement> = document.getElementsByTagName("p");
 for (let i = 0; i < ps.length; i++) {
-    ps[i].style.color = "red";
-    ps[i].innerText = "Hello, World!";
+    const p: HTMLParagraphElement = ps[i];
+    p.style.color = "red";
+    p.innerText = "Hello, World!";
+    p.innerText.replace
 }
